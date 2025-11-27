d97df849d598ffd6b1796ba45796f27c3cb07832
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-11-26 22:48:28 -0600

    fix: configuramos todos los archivos necesarios para desarrollo y produccion

--- src/main/java/mx/nube2024/ia/app/htsoft/HelloController.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/HelloController.java
@@ -1,23 +1,25 @@
 package mx.nube2024.ia.app.htsoft;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController
 {
-
-    private final htsoftAIServive aiService;
+    private final htsoftAIService aiService;
+    private final String platform;
 
     /*Inyectamos la dependencia de la interfaz a traves del constructor*/
-    public HelloController(htsoftAIServive aiService) {
+    public HelloController(htsoftAIService aiService, @Value("${spring.application.name}") String platform) {
         this.aiService = aiService;
+        this.platform = platform;
     }
 
     @GetMapping("/")
     public String hello()
     {
-        return this.aiService.generatedGreeting();
+        return this.aiService.generatedGreeting(this.platform);
     }
 }
--- src/main/java/mx/nube2024/ia/app/htsoft/htsoftAIService.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/htsoftAIService.java
@@ -1,14 +1,15 @@
 package mx.nube2024.ia.app.htsoft;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
-public interface htsoftAIServive {
+public interface htsoftAIService {
 
     @UserMessage("""
             Genera un saludo de bienvenida a la plataforma de Gestion de Peliculas Htsoft
-            usa menos de 120 caracteres y hazlo con el estylo de platzi
+            usa menos de 120 caracteres y hazlo con el estylo de {{platform}}
             """)
-    String generatedGreeting();
+    String generatedGreeting(@V("platform") String platform);
 }
