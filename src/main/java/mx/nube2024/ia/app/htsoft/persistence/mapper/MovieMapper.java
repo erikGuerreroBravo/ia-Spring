c775efc7a905da12d7b407f5baa9a6ee0ac4f492
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-02 13:08:29 -0600

    fix: empezamos por mapear campo por campo para la interaccion de los elementos d ela clase.

--- src/main/java/mx/nube2024/ia/app/htsoft/persistence/mapper/MovieMapper.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/persistence/mapper/MovieMapper.java
@@ -1,7 +1,12 @@
 package mx.nube2024.ia.app.htsoft.persistence.mapper;
 
+import mx.nube2024.ia.app.htsoft.domain.dto.MovieDto;
+import mx.nube2024.ia.app.htsoft.persistence.entity.MovieEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MovieMapper {
+    @Mapping(source="titulo", target="title")
+    MovieDto toDto(MovieEntity entity);
 }
