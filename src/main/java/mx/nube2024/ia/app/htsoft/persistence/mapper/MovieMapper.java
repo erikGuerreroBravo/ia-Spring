ab77e66bafdebef48a96c3303003520e815f6a89
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-02 13:12:49 -0600

    fix:modificamos las propiedades entre dto y  entity para mapeo de los campos de forma correcta.

--- src/main/java/mx/nube2024/ia/app/htsoft/persistence/mapper/MovieMapper.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/persistence/mapper/MovieMapper.java
@@ -8,5 +8,9 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface MovieMapper {
     @Mapping(source="titulo", target="title")
+    @Mapping(source="duracion", target="duration")
+    @Mapping(source="genero", target="genere")
+    @Mapping(source="fechaEstreno", target="releaseDate")
+    @Mapping(source="clasificacion", target="rating")
     MovieDto toDto(MovieEntity entity);
 }
