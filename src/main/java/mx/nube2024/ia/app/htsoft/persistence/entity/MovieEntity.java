8c9ea20aaa53e86e25a53638e249467385af9ad7
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-01 23:26:35 -0600

    fix:se generan los metodos setter y getter de la clase de entidad, utilizando los elementos de las propiedades generadas en la entidad

--- src/main/java/mx/nube2024/ia/app/htsoft/persistence/entity/MovieEntity.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/persistence/entity/MovieEntity.java
@@ -1,8 +1,62 @@
 package mx.nube2024.ia.app.htsoft.persistence.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class MovieEntity {
 
     private String titulo;
     private Integer duracion;
     private String genero;
+    private LocalDate fechaEstreno;
+    private BigDecimal clasificacion;
+    private String estado;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public LocalDate getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(LocalDate fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+
+    public BigDecimal getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(BigDecimal clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
