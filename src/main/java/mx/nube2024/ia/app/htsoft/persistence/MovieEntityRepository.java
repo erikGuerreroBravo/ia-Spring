ac1d177a98f63df23b6bd53ea5632d3bb05968d9
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-02 12:57:12 -0600

    feat: agregamos MovieEntityRepository a nuestro proyecto para la implementacion de los metodoos declarados en la interfaz

---
+++ src/main/java/mx/nube2024/ia/app/htsoft/persistence/MovieEntityRepository.java
@@ -0,0 +1,13 @@
+package mx.nube2024.ia.app.htsoft.persistence;
+
+import mx.nube2024.ia.app.htsoft.domain.dto.MovieDto;
+import mx.nube2024.ia.app.htsoft.domain.repository.MovieRepository;
+
+import java.util.List;
+
+public class MovieEntityRepository implements MovieRepository {
+    @Override
+    public List<MovieDto> getAll() {
+        return null;
+    }
+}
