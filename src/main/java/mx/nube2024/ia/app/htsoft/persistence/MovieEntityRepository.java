6f1ae7e23bae5190fb0ad1c04bac58353be2e753
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-02 12:59:45 -0600

    feat: agregamos MovieEntityRepository con la notacion @Repository para indicar que es un repositorio de datos.

--- src/main/java/mx/nube2024/ia/app/htsoft/persistence/MovieEntityRepository.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/persistence/MovieEntityRepository.java
@@ -2,10 +2,22 @@ package mx.nube2024.ia.app.htsoft.persistence;
 
 import mx.nube2024.ia.app.htsoft.domain.dto.MovieDto;
 import mx.nube2024.ia.app.htsoft.domain.repository.MovieRepository;
+import mx.nube2024.ia.app.htsoft.persistence.crud.CrudMovieEntity;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+//tiene que ser un repository para inyectar las clases que se ocupan dentro de
+//este repositorio.
+@Repository
 public class MovieEntityRepository implements MovieRepository {
+
+    private final CrudMovieEntity crudMovieEntity;
+
+    public MovieEntityRepository(CrudMovieEntity crudMovieEntity) {
+        this.crudMovieEntity = crudMovieEntity;
+    }
+
     @Override
     public List<MovieDto> getAll() {
         return null;
