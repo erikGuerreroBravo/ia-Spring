e6977f3d2491aa6d7ea085141a1ba196e50cd611
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-02 01:08:28 -0600

    fix:moidificamos archivos de propeidades para realziar la carga inicial de datos en sql.

---
+++ src/main/java/mx/nube2024/ia/app/htsoft/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,7 @@
+package mx.nube2024.ia.app.htsoft.persistence.crud;
+
+import mx.nube2024.ia.app.htsoft.persistence.entity.MovieEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrudMovieEntity extends CrudRepository<MovieEntity,Long> {
+}
