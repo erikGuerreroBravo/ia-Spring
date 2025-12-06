b5f6101df5f31039a858dcd8b6767336ddab34a6
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-06 17:44:13 -0600

    fix:modificamos el parametro de consulta de los controladores.

--- src/main/java/mx/nube2024/ia/app/htsoft/web/controller/MovieController.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/web/controller/MovieController.java
@@ -4,11 +4,13 @@ import mx.nube2024.ia.app.htsoft.domain.dto.MovieDto;
 import mx.nube2024.ia.app.htsoft.domain.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
     private final MovieService movieService;
@@ -17,13 +19,13 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/movies")
+    @GetMapping
     public List<MovieDto> getAll()
     {
         //casteamos el resultado al tipo de objeto que se necesita transformar
         return  this.movieService.getAll();
     }
-    @GetMapping("/movies/{id}")
+    @GetMapping("/{id}")
     public MovieDto getById(@PathVariable Long id){
         return null;
     }
