763d4f08b80405280f05d666f93fbb7048d864c6
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-11 15:36:52 -0600

    fix:modificamos la clase crudmovieEntiity agregando un metodo de busqueda

--- src/main/java/mx/nube2024/ia/app/htsoft/persistence/crud/CrudMovieEntity.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/persistence/crud/CrudMovieEntity.java
@@ -4,4 +4,6 @@ import mx.nube2024.ia.app.htsoft.persistence.entity.MovieEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CrudMovieEntity extends CrudRepository<MovieEntity,Long> {
+
+    MovieEntity findFirstByTitle(String titulo);
 }
