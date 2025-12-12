932f98506b9fd42fd595acf90a1c33636caeaabf
erikGuerreroBravo <erik.guerrero.bravo@hotmail.com>
2025-12-12 00:02:53 -0600

    feat:agregamos nuevo elemento de validacion para una lista generica de errores.

--- src/main/java/mx/nube2024/ia/app/htsoft/web/excepcion/RestExceptionHandler.java
+++ src/main/java/mx/nube2024/ia/app/htsoft/web/excepcion/RestExceptionHandler.java
@@ -2,8 +2,12 @@ package mx.nube2024.ia.app.htsoft.web.excepcion;
 
 import mx.nube2024.ia.app.htsoft.domain.exception.MovieAlreadyExistException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 //clase encargada de capturar las a excepciones que ocurran en la peticion
@@ -16,4 +20,16 @@ public class RestExceptionHandler
         Error error= new Error("movie-already-existss",ex.getMessage());
         return ResponseEntity.badRequest().body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Error>> handleExceptin(MethodArgumentNotValidException ex)
+    {
+         List<Error> errors = new ArrayList<>();
+         ex.getBindingResult().getFieldErrors().forEach(error->{
+             errors.add(new Error(error.getField(),error.getDefaultMessage()));
+         });
+         return ResponseEntity.badRequest().body(errors);
+    }
+
+
 }
